3e280f849c5341c7011e48772d88818a91db8c1d
Wolf <kubica_lukasz@wp.pl>
2022-01-22 16:59:16 +0100

    initial commit

---
+++ src/main/java/lucqubica/quiz/rest/HealthcheckRestController.java
@@ -0,0 +1,15 @@
+package lucqubica.quiz.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/health")
+public class HealthcheckRestController {
+
+    @GetMapping
+    public String heathcheck() {
+        return "It's working";
+    }
+}
